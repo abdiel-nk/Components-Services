fce3143234a8fd68e1850051741db35e65c71d7a
AbdielMamani <abdielmamani@upeu.edu.pe>
2023-02-01 21:54:41 -0500

    dowloand and upload files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {environment} from './../environments/environment';
 import {AuthService } from './services/auth.service';
 import {UsersService } from './services/users.service';
+import {FilesService} from './services/files.service';
 
 @Component({
   selector: 'app-root',
@@ -12,9 +13,11 @@ export class AppComponent {
   imgParent = '';
   showImg = true;
   token = '';
+  imgRta = '';
 
   constructor(
-  private usersService: UsersService
+  private usersService: UsersService,
+  private filesService: FilesService
   ){  }
 
   onLoaded(){
@@ -30,7 +33,20 @@ export class AppComponent {
       password : 'alfred123'
     }).subscribe(rta=>{
       console.log(rta);
-
     });
+  }
+//uses `` instead of ''
+  downloadfile(){
+    this.filesService.getFile(`my.pdf`,`https://young-sands-07814.herokuapp.com/api/files/dummy.pdf
+`,`application/pdf`).subscribe()
+  }
+  onUpload(event : Event){
+    const element = event.target as HTMLInputElement;
+    const file =  element.files?.item(0);
+    if(file){
+      this.filesService.uploadFile(file).subscribe(rta => {
+        this.imgRta = rta.location;
+      });
+    }
   }
 }
