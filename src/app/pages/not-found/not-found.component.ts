3536da27724133118c4d8bd47ee392e577041930
AbdielMamani <abdielmamani@upeu.edu.pe>
2023-02-16 15:23:01 -0500

    page-not-found

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
-  templateUrl: './not-found.component.html',
+  template: `<h1>Page Not Found</h1>
+    <div>
+      <img src="https://media.giphy.com/media/A9EcBzd6t8DZe/giphy.gif">
+    </div>`,
   styleUrls: ['./not-found.component.scss']
 })
 export class NotFoundComponent {
